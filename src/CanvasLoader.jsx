4b76d39c990059285288302042f2d730fc88dbf2
Sushil G <guptask.7@gmail.com>
2023-09-03 12:36:56 -0400

    Fixed loading component and style to show on proper colours

--- src/CanvasLoader.jsx
+++ src/CanvasLoader.jsx
@@ -5,22 +5,33 @@ function CanvasLoader () {
     const { progress } = useProgress();
 
     return (
-       <Html
-        as='div'
-        center
-        style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            flexDirection: "column"
-        }}
-       >
+        <Html
+            as='div'
+            center
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column"
+            }}
+        >
+        
+        <p
+            style={{
+                fontSize: 40,
+                color: "#F1F1F1",
+                fontWeight: 800,
+                marginTop: 40
+            }}
+        >
+            Loading...<br />
+        </p>
         
         <span className='canvas-loader'></span>
 
         <p
             style={{
-                fontSize: 14,
+                fontSize: 40,
                 color: "#F1F1F1",
                 fontWeight: 800,
                 marginTop: 40
